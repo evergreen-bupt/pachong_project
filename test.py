c8d1d9ce64296ead847a9ffa983e96d9cb53d2a5
liang <1374203630@qq.com>
2023-11-09 17:43:14 +0800

    zk update 优化程序断点存储

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 a = '48h'
 b = a.split("h")
 print(b)
+=======
+import json
+
+information_dict = {'x':1,'y':2}
+with open("eval_information.json", "w", encoding='utf-8') as f:
+            json.dump(information_dict, f, indent=2, sort_keys=True, ensure_ascii=False) 
+>>>>>>> a624bf59ed273e253c032a3993b0565922aa81f2
